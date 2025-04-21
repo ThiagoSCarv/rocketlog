459f454fd26988324a448349fa770ceb872b1cde
Thiago Soares <thiagocarv4@gmail.com>
2025-04-21 19:47:01 -0300

    Validando Dados do Usuário

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
 
 class UsersControllers {
   async create(resquest: Request, response: Response, next: NextFunction) {
-    return response.json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(resquest.body);
+
+    return response.json({ name, email, password});
   }
 }
 
