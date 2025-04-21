ac04ae2f955f1f62f6d28559564eebef8df37e7d
Thiago Soares <thiagocarv4@gmail.com>
2025-04-21 20:41:30 -0300

    Finalizando Retorno do Usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -34,7 +34,9 @@ class SessionsControllers {
       expiresIn,
     });
 
-    return response.json({ token });
+    const { password: hashedPassword, ...userWithoutPassword } = user;
+
+    return response.json({ token, user: userWithoutPassword });
   }
 }
 
