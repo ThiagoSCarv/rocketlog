d493d68ef9373db616a302e887bae267cc59c62c
Thiago Soares <thiagocarv4@gmail.com>
2025-04-22 20:10:41 -0300

    Cadastra Log

--- src/controllers/delivery-logs-controllers.ts
+++ src/controllers/delivery-logs-controllers.ts
@@ -1,7 +1,38 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/app-error";
 
 export class DeliveryLogsControllers {
   async create(request: Request, response: Response) {
-    return response.json({message: "ok"})
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: {
+        id: delivery_id,
+      },
+    });
+
+    if (!delivery) {
+      throw new AppError("delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped");
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
   }
 }
