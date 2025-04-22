1c3dc9e24e51d9c2d4c722398e679e450abbbc75
Thiago Soares <thiagocarv4@gmail.com>
2025-04-22 18:02:38 -0300

    Verificando Autorização

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { DeliveriesControllers } from "@/controllers/deliveries-controllers";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const deliveriesRoutes = Router();
 const deliveriesControllers = new DeliveriesControllers();
 
-deliveriesRoutes.use(ensureAuthenticated)
+deliveriesRoutes.use(
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale"])
+);
 
 deliveriesRoutes.post("/", deliveriesControllers.create);
 
